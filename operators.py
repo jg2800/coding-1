190f71bd7bb99f2c05c4db308d6f1707c7c0c33f
BBYFACEJAI <jgurley841@boyslatin.net>
2026-09-09 14:39:15 +0000

    operator.py

---
+++ operators.py
@@ -0,0 +1,40 @@
+# Assignment operrators- Exclusively used to 
+# assign values to variables
+# Think key value pairing 
+
+# we used a single equal sign to represent the assignment 
+# operrator 
+name= "Jai"
+grade=10
+school= True 
+
+
+# Arithmetic operrators- used or numerical 
+# data types to perform calculations.
+# intergers ( whole numbers) and floats ( decimal number)
+
+# print is a function that lets us show code 
+# in the terminal
+print ( 3+3) # addition operator 
+print (3.9 - 3) # subtraction operator
+print ( 11 / 3 ) #division operator 
+print (2800 * 1500)
+print  ( 10 > 1 ) #greater than poerator 
+print ( 10 < 1 ) # less oprator 
+
+# 2 a equal signs compare is spmething is the same 
+print "( Jai = Jai )"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
