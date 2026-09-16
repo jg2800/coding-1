88963b59a5e318b8c02ec96c5dcb92955a4c03e5
BBYFACEJAI <jgurley841@boyslatin.net>
2026-09-16 14:15:51 +0000

    day2 activity

--- unit 1/operatorsActivity.py
+++ unit 1/operatorsActivity.py
@@ -6,3 +6,16 @@ print ( "coding 1" == " coding1 ") # false
 
 print ( " 0"  != 0 )  # true
 
+ # day2 activity
+
+ print (book + tablet) # true
+
+print (cart + amouttogetdiscount) # true
+
+print (shoes + tax) # true
+
+print (GPA + recommendation) # true
+
+print (1 + 2)   # false 
+
+
